bab1153a866de9949cce8a1f61374cfee45b4374
Babatunde Adeyemi <tundewrites@gmail.com>
2017-06-25 05:13:53 +0100

    Install express-session middleware

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import session from 'express-session';
 
 import userRoutes from './routes/userRoutes';
 import groupRoutes from './routes/groupRoutes';
@@ -9,6 +10,12 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: 'dui8w9837@##&*(#&(*#*(@bdhdfAJKHKJHJAHJ',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use('/api', userRoutes);
 app.use('/api', groupRoutes);
