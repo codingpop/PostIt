002c6ff7a9c6a038ce4f9def29ef8e5620eb4e57
Babatunde Adeyemi <tundewrites@gmail.com>
2017-06-25 05:34:49 +0100

    Set up dotenv

--- server/app.js
+++ server/app.js
@@ -1,18 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import session from 'express-session';
+import dotenv from 'dotenv';
+
 
 import userRoutes from './routes/userRoutes';
 import groupRoutes from './routes/groupRoutes';
 
+dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(session({
-  secret: 'dui8w9837@##&*(#&(*#*(@bdhdfAJKHKJHJA',
+  secret: process.env.SESS_SECRET,
   resave: false,
   saveUninitialized: true
 }));
@@ -20,4 +22,4 @@ app.use(session({
 app.use('/api', userRoutes);
 app.use('/api', groupRoutes);
 
-app.listen(port);
+app.listen(process.env.PORT);
