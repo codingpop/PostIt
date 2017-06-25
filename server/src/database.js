fc228b8466171294e52aafd90dfecd48564d21dd
Babatunde Adeyemi <tundewrites@gmail.com>
2017-06-25 05:10:18 +0100

    Fix all relationship issues

--- server/src/database.js
+++ server/src/database.js
@@ -2,7 +2,9 @@ import dotenv from 'dotenv';
 import Sequelize from 'sequelize';
 import User from './../models/user';
 import Group from './../models/group';
-
+import Message from './../models/message';
+import GroupMember from './../models/groupMember';
+import Archive from './../models/archive';
 
 dotenv.config();
 
@@ -21,7 +23,9 @@ database.Sequelize = Sequelize;
 database.connection = connection;
 database.User = User(connection, Sequelize);
 database.Group = Group(connection, Sequelize);
-
+database.Message = Message(connection, Sequelize);
+database.GroupMember = GroupMember(connection, Sequelize);
+database.Archive = Archive(connection, Sequelize);
 
 // Database 1:M relationships
 database.Group.belongsTo(database.User, {
@@ -29,6 +33,41 @@ database.Group.belongsTo(database.User, {
 });
 database.User.hasMany(database.Group, {
   foreignKey: 'userId'
+});
+
+database.Message.belongsTo(database.Group, {
+  foreignKey: 'inGroup'
+});
+database.Group.hasMany(database.Message, {
+  foreignKey: 'inGroup'
+});
+
+database.Message.belongsTo(database.User, {
+  foreignKey: 'author'
+});
+database.User.hasMany(database.Message, {
+  foreignKey: 'author'
+});
+
+// Database N:M relationships
+database.User.belongsToMany(database.Group, {
+  through: database.GroupMember,
+  foreignKey: 'userId'
+});
+
+database.Group.belongsToMany(database.User, {
+  through: database.GroupMember,
+  foreignKey: 'groupId'
+});
+
+database.User.belongsToMany(database.Message, {
+  through: database.Archive,
+  foreignKey: 'userId'
+});
+
+database.Message.belongsToMany(database.User, {
+  through: database.Archive,
+  foreignKey: 'messageId'
 });
 
 export default database;
