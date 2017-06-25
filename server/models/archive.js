129b13dc06c332ea2dd3b9421e4b16ab4da5f8f4
Babatunde Adeyemi <tundewrites@gmail.com>
2017-06-25 05:05:25 +0100

    Add archive model

--- server/models/archive.js
+++ server/models/archive.js
@@ -5,6 +5,16 @@ export default (connection, Sequelize) => {
       defaultValue: Sequelize.UUIDV4,
       primaryKey: true
     },
+    // References messageId in the message model
+    messageId: {
+      type: Sequelize.UUID,
+      allowNull: false
+    },
+    // References userId in the user model
+    userId: {
+      type: Sequelize.UUID,
+      allowNull: false
+    }
   }, {
     paranoid: true
   });
