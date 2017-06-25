718f2b94aaf58d47a6df7fe8b94662218e7cbda7
Babatunde Adeyemi <tundewrites@gmail.com>
2017-06-25 05:06:32 +0100

    Add groupMember (group membership) model

--- server/models/groupMember.js
+++ server/models/groupMember.js
@@ -1,10 +1,16 @@
 export default (connection, Sequelize) => {
   const GroupMember = connection.define('groupMember', {
+    membershipId: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true
+    },
+    // References userId in the User model
     userId: {
       type: Sequelize.UUID,
       allowNull: false,
     },
-    // References userId in User Model
+    // References groupId in the Group Model
     groupId: {
       type: Sequelize.UUID,
       allowNull: false
