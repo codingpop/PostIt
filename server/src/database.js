0e106c85948354fac279cf65fcf8572c5477ead9
tundewritescode <tundewrites@gmail.com>
2017-09-13 11:41:51 +0100

    fix missing files

--- server/src/database.js
+++ server/src/database.js
@@ -8,34 +8,19 @@ import Archive from './../models/archive';
 
 dotenv.config();
 
-let connection;
-
-if (process.env.NODE_ENV !== 'production') {
-  connection = new Sequelize(
-    process.env.DB_TEST_NAME,
-    process.env.DB_TEST_USER,
-    process.env.DB_TEST_PASS, {
-      host: process.env.DB_TEST_HOST,
-      port: process.env.DB_PORT,
-      dialect: 'postgres',
-      logging: false
-    });
-} else {
-  connection = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS, {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: 'postgres',
-      dialectOptions: {
-        ssl: true,
-        native: true,
-      },
-      logging: false
-    });
-}
-
+const connection = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: true,
+      native: true,
+    },
+    logging: false
+  });
 
 const database = {};
 
